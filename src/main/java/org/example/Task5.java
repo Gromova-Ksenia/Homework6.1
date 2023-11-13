57e3b8640f16722a36e36511e71f0d36b1538d00
Герди <@DESKTOP-LKI8143>
2023-11-13 03:28:32 +0300

    Initial commit

---
+++ src/main/java/org/example/Task5.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int dragonAtack,
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
